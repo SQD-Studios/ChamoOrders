f2b1d96f0454c980cc906b37ff29d46648fa8026
Chamogelastos <stavros.ssta@gmail.com>
2026-03-31 14:08:37 +0300

    Every imlement works

--- src/main/java/net/chamosmp/net/ChamoOrders/paper/PaperMain.java
+++ src/main/java/net/chamosmp/net/ChamoOrders/paper/PaperMain.java
@@ -4,20 +4,117 @@ package net.chamosmp.net.ChamoOrders.paper;
 import net.chamosmp.net.ChamoOrders.paper.Commands.SimpleCommand;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
-import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
+import net.milkbowl.vault.permission.Permission;
+
 public class PaperMain extends JavaPlugin implements Listener {
+
+    private static Economy econ = null;
+    private static Permission perms = null;
+    private static Chat chat = null;
+
+    public static JavaPlugin get() {
+        return null;
+    }
+
     @Override
     public void onEnable() {
+        //Open Universal and methods
+
         Bukkit.getPluginManager().registerEvents(this, this);
+        //Bstats
         int pluginId = 30390;
         Metrics metrics = new Metrics(this, pluginId);
-
-        // Optional: Add custom charts
-        //metrics.addCustomChart();
-        //  BasicCommand yourCommand = "order";
+        //Command Register
         registerCommand("order", new SimpleCommand());
+
+        //Vault
+        if (!setupEconomy() ) {
+            getLogger().severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        setupPermissions();
+        setupChat();
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info(String.format("[%s] Disabled Version %s", getDescription().getName(), getDescription().getVersion()));
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    private boolean setupChat() {
+        RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
+        chat = rsp.getProvider();
+        return chat != null;
+    }
+
+    private boolean setupPermissions() {
+        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+        perms = rsp.getProvider();
+        return perms != null;
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
+        if(!(sender instanceof Player)) {
+            getLogger().info("Only players are supported for this Example Plugin, but you should not do this!!!");
+            return true;
+        }
+
+        Player player = (Player) sender;
+
+        if(command.getLabel().equals("test-economy")) {
+            // Lets give the player 1.05 currency (note that SOME economic plugins require rounding!)
+            sender.sendMessage(String.format("You have %s", econ.format(econ.getBalance(player.getName()))));
+            EconomyResponse r = econ.depositPlayer(player, 1.05);
+            if(r.transactionSuccess()) {
+                sender.sendMessage(String.format("You were given %s and now have %s", econ.format(r.amount), econ.format(r.balance)));
+            } else {
+                sender.sendMessage(String.format("An error occured: %s", r.errorMessage));
+            }
+            return true;
+        } else if(command.getLabel().equals("test-permission")) {
+            // Lets test if user has the node "example.plugin.awesome" to determine if they are awesome or just suck
+            if(perms.has(player, "example.plugin.awesome")) {
+                sender.sendMessage("You are awesome!");
+            } else {
+                sender.sendMessage("You suck!");
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static Economy getEconomy() {
+        return econ;
+    }
+
+    public static Permission getPermissions() {
+        return perms;
+    }
+
+    public static Chat getChat() {
+        return chat;
     }
 }
